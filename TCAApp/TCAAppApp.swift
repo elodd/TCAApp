3a770a6729557e07c4f63242f93b207f8d55fc33
Elöd Dobos <elod.dobos.extern@skanetrafiken.com>
2024-02-09 10:39:28 +0100

    Initial Commit

---
+++ TCAApp/TCAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAppApp.swift
+//  TCAApp
+//
+//  Created by Elöd Dobos on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct TCAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
